af6b80856d20be956d142d1dde31f5b3a078d8f9
Nimesh Lakshitha <mishman.dev@gmail.com>
2025-11-09 18:37:15 +0530

    Change Settings for Production

--- InventoryMS/settings.py
+++ InventoryMS/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h1kcco)a9drn5vwk-f%8c)fxuqh7w6i@p7qqu-d9n1bx)t+v7d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
